f3229cd3c30ce03fe7c5afd25f4c8ffc45b369db
Limoncello <bloogymonster000@gmail.com>
2026-08-22 01:13:28 +0200

    feat: add health endpoint

--- beckend/cmd/server/main.go
+++ beckend/cmd/server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, `{"status":"ok"}`)
+}
 
 func main() {
-	fmt.Println("Routine Dungeon server")
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Println("Server started on :8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
