ef01b8a23f23185f12b2a7c5a171eeed7b15cd2d
Limoncello <limoncello@Limoncello.local>
2026-08-27 23:33:31 +0200

    fix dungeon response with tasks

--- beckend/internal/dungeon/model.go
+++ beckend/internal/dungeon/model.go
@@ -13,12 +13,13 @@ type Dungeon struct {
 	CreatedAt       time.Time `json:"created_at"`
 	Status          bool      `json:"status"`
 	RoutineID       int       `json:"routine_id"`
+	Tasks           []Task    `json:"tasks"`
 }
 
 type Task struct {
-	ID 		int    `json:"id"`
-	Title 	string `json:"title"`
-	Damage 	int    `json:"damage"`
-	Completed bool  `json:"completed"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Damage    int    `json:"damage"`
+	Completed bool   `json:"completed"`
 	DungeonID int    `json:"dungeon_id"`
 }
